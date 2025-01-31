499a596f47f0b43f3d90f48407e11b9c5ae87e02
dthv201 <gaya.vishna@gmail.com>
2025-01-31 11:29:03 +0200

    Update import path for WelcomePage component in App.tsx

--- football_front/src/App.tsx
+++ football_front/src/App.tsx
@@ -1,5 +1,5 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import WelcomePage from "./pages/WelcomePage";
+import WelcomePage from "./components/WelcomePage";
 
 function App() {
   return (
