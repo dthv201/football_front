7b41e06e3156e29198bcdcf329c701560cd75f98
Lior Oliel <Lior.Oliel20.github@gmail.com>
2025-03-22 21:50:32 +0200

    bugfix: handleGenerateTeams, handleLeaveGame, handleJoinGame

--- football_front/src/Pages/GameInfo.tsx
+++ football_front/src/Pages/GameInfo.tsx
@@ -1,13 +1,12 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useCallback } from "react";
 import { useParams } from "react-router-dom";
 import Layout from "../components/page_tamplate/Layout";
-// import { addParticipant, removeParticipant, splitParticipantsIntoTeams } from "../services/postService";
-import { addParticipant, getPost, removeParticipant } from "../services/postService";
+import { addParticipant, removeParticipant, splitParticipantsIntoTeams, getPost } from "../services/postService";
 import { Post } from "../types/Post";
 import { User } from "../types/User";
-import { axiosInstance } from "../services/api-client";
 import { useUserContext } from "../contexts/UserContext";
-import { Box, Typography, Button, Grid, Paper, Divider, List, ListItem, ListItemText } from "@mui/material";
+import { Box, Typography, Button, Paper, Divider, List, ListItem, ListItemText } from "@mui/material";
+import Grid from "@mui/material/Grid2";
 import SportsSoccerIcon from "@mui/icons-material/SportsSoccer";
 import { getParticipants } from "../services/auth";
 
@@ -16,68 +15,91 @@ const GameInfoPage: React.FC = () => {
     const { postId } = useParams<{ postId: string }>();
     const [post, setPost] = useState<Post | null>(null);
     const [participants, setParticipants] = useState<User[]>([]);
-    // const [teamsGenerated, setTeamsGenerated] = useState(false);
-    // const [teamA, setTeamA] = useState<string[]>([]);
-    // const [teamB, setTeamB] = useState<string[]>([]);
+    const [teamsGenerated, setTeamsGenerated] = useState(false);
+    const [teamA, setTeamA] = useState<User[]>([]);
+    const [teamB, setTeamB] = useState<User[]>([]);
 
-    useEffect(() => {
-        const fetchPost = async () => {
-            try {
-                if(postId) {
-                const post = await getPost(postId);
-                setPost(post);
-                
-                if (post?.participantsIds?.length) {
-                    const participants = await getParticipants(post.participantsIds);
-                    setParticipants(participants);
-                }
-            }
-            } catch (error) {
-                console.error("Error fetching post:", error);
-            }
-        };
-        fetchPost();
-    }, [postId]);
 
-    const handleJoinGame = async () => {
+    const handleJoinGame = useCallback(async () => {
         try {
             await addParticipant(postId!, user!._id);
-            const response = await axiosInstance.get<Post>(`/posts/${postId}`);
-            setPost(response.data);
+            const post = await getPost(postId!);
+            setPost(post);
+
+            if (post?.participantsIds?.length) {
+                const participants = await getParticipants(post.participantsIds);
+                setParticipants(participants);
+            }
+            alert("Participant added successfully");
         } catch (error) {
             console.error("Error joining game:", error);
         }
-    };
+    }, [postId, user]);
 
-    const handleLeaveGame = async () => {
+    const handleLeaveGame = useCallback(async () => {
         try {
             await removeParticipant(postId!, user!._id);
-            const response = await axiosInstance.get<Post>(`/posts/${postId}`);
-            setPost(response.data);
+            const post = await getPost(postId!);
+            setPost(post);
+            if (post?.participantsIds?.length) {
+                const participants = await getParticipants(post.participantsIds);
+                setParticipants(participants);
+                setTeamA(participants.filter(p => post.teamA?.includes(p._id)));
+                setTeamB(participants.filter(p => post.teamB?.includes(p._id)));
+            }
+            alert("Participant removed successfully");
         } catch (error) {
             console.error("Error leaving game:", error);
         }
-    };
+    }, [postId, user]);
 
-    // const handleGenerateTeams = async () => {
-    //     if (teamsGenerated) return;
-        
-    //     try {
-    //         const { GenerateTeamsResponse } = await splitParticipantsIntoTeams(postId!);
-    //         setTeamA(teamA);
-    //         setTeamB(teamB);
-    //         setTeamsGenerated(true);
-    //     } catch (error) {
-    //         console.error("Error generating teams:", error);
-    //     }
-    // };
+    const fetchPost = useCallback(async () => {
+        try {
+            if (postId) {
+                const post = await getPost(postId);
+                setPost(post);
+
+                if (post?.participantsIds?.length) {
+                    const participants = await getParticipants(post.participantsIds);
+                    setParticipants(participants);
+                    setTeamA(participants.filter(p => post.teamA?.includes(p._id)));
+                    setTeamB(participants.filter(p => post.teamB?.includes(p._id)));
+                }
+            }
+        } catch (error) {
+            console.error("Error fetching post:", error);
+        }
+    }, [postId]);
+
+    const handleGenerateTeams = useCallback(async () => {
+        if (teamsGenerated) return;
+
+        try {
+            const postWithSplitTeams = await splitParticipantsIntoTeams(postId!);
+            setPost(postWithSplitTeams);
+            if (postWithSplitTeams?.participantsIds?.length) {
+                const participants = await getParticipants(postWithSplitTeams.participantsIds);
+                setParticipants(participants);
+                setTeamA(participants.filter(p => postWithSplitTeams.teamA?.includes(p._id)));
+                setTeamB(participants.filter(p => postWithSplitTeams.teamB?.includes(p._id)));
+            }
+            setTeamsGenerated(true);
+            alert("Teams generated successfully!");
+        } catch (error) {
+            console.error("Error generating teams:", error);
+        }
+    }, [postId, teamsGenerated]);
+
+    useEffect(() => {
+        fetchPost();
+    }, [fetchPost, handleJoinGame, handleLeaveGame, handleGenerateTeams]);
 
     if (!post) return <Layout title="Game Details">Loading...</Layout>;
 
     return (
         <Layout title="Game Details">
-            <Grid container spacing={4}>
-                <Grid item xs={12} md={6}>
+            <Grid container spacing={2}>
+                <Grid size={6}>
                     <Paper sx={{ p: 3, borderRadius: 2, boxShadow: 3 }}>
                         <Typography variant="h5" fontWeight="bold" gutterBottom>
                             {post.title}
@@ -92,44 +114,47 @@ const GameInfoPage: React.FC = () => {
                         <Typography variant="body2">{post.content}</Typography>
                     </Paper>
                 </Grid>
-
-                <Grid item xs={12} md={6}>
+                <Grid size={2}>
                     <Paper sx={{ p: 3, borderRadius: 2, boxShadow: 3 }}>
                         <Typography variant="h6" fontWeight="bold">Participants:</Typography>
+                        <Divider sx={{ my: 1 }} />
                         <List>
                             {participants?.map((participant) => (
                                 <ListItem key={participant._id}>
-                                    <ListItemText primary={`${participant.username} - ${participant.skillLevel}`} />
+                                    <ListItemText primary={participant.username} secondary={participant.skillLevel} />
                                 </ListItem>
                             ))}
                         </List>
-                        
-                        {/* {teamsGenerated && (
-                            <>
-                                <Divider sx={{ my: 2 }} />
-                                <Typography variant="h6" fontWeight="bold" color="primary">
-                                    Team A ⚽
-                                </Typography>
-                                <List>
-                                    {teamA.map((player) => (
-                                        <ListItem key={player}>
-                                            <ListItemText primary={player} />
-                                        </ListItem>
-                                    ))}
-                                </List>
-                                <Divider sx={{ my: 2 }} />
-                                <Typography variant="h6" fontWeight="bold" color="secondary">
-                                    Team B ⚽
-                                </Typography>
-                                <List>
-                                    {teamB.map((player) => (
-                                        <ListItem key={player}>
-                                            <ListItemText primary={player} />
-                                        </ListItem>
-                                    ))}
-                                </List>
-                            </>
-                        )} */}
+                    </Paper>
+                </Grid>
+                <Grid size={2}>
+                    <Paper sx={{ p: 3, borderRadius: 1, boxShadow: 1 }}>
+                        <Typography variant="h6" fontWeight="bold" color="primary">
+                            Team A ⚽
+                        </Typography>
+                        <Divider sx={{ my: 1 }} />
+                        <List>
+                            {teamA.map((player) => (
+                                <ListItem key={player._id}>
+                                    <ListItemText primary={player.username} secondary={player.skillLevel}/>
+                                </ListItem>
+                            ))}
+                        </List>
+                    </Paper>
+                </Grid>
+                <Grid size={2}>
+                    <Paper sx={{ p: 3, borderRadius: 1, boxShadow: 1 }}>
+                        <Typography variant="h6" fontWeight="bold" color="secondary">
+                            Team B ⚽
+                        </Typography>
+                        <Divider sx={{ my: 1 }} />
+                        <List>
+                            {teamB.map((player) => (
+                                <ListItem key={player._id}>
+                                    <ListItemText primary={player.username} secondary={player.skillLevel} />
+                                </ListItem>
+                            ))}
+                        </List>
                     </Paper>
                 </Grid>
             </Grid>
@@ -141,14 +166,14 @@ const GameInfoPage: React.FC = () => {
                 <Button variant="contained" color="error" onClick={handleLeaveGame}>
                     Leave Game
                 </Button>
-                {/* <Button 
-                    variant="contained" 
-                    color="success" 
+                <Button
+                    variant="contained"
+                    color="success"
                     onClick={handleGenerateTeams}
                     disabled={teamsGenerated}
                 >
                     Generate Teams
-                </Button> */}
+                </Button>
             </Box>
         </Layout>
     );
